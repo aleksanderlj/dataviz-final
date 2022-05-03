4aa6f8b979e29e2ef24012f00b02cdc4bef79f12
aleksanderlj <aleksanderlj@hotmail.com>
2022-05-03 15:00:14 +0200

    Smoothscroll polyfill

--- src/pages/caucasian/CaucasianMain.tsx
+++ src/pages/caucasian/CaucasianMain.tsx
@@ -1,4 +1,4 @@
-import {Box, Grid, Stack} from "@mui/material";
+import {Box, Grid, Stack, Typography} from "@mui/material";
 import FoliumMap from "../components/FoliumMap";
 import Image from "mui-image"
 import FeatureImportance from "../../images/plots/Percent_of_total_enrollment_that_are_White.png"
@@ -23,8 +23,13 @@ function CaucasianMain() {
         <Grid container justifyContent={"center"}>
             <Grid container item xs={8} justifyContent={"center"} rowSpacing={4}>
                 <Grid item xs={8}>
-                    {/* <FoliumMap src={"dataviz-final/plots/map_white.html"} /> */}
-                    <FoliumMap src={"plots/map_white.html"} />
+                    <Typography variant={"h6"}>
+                        {"Under- and overrepresentation in each state"}
+                    </Typography>
+                    {window.location.hostname === "localhost" ?
+                        <FoliumMap src={"dataviz-final/plots/map_white.html"} /> :
+                        <FoliumMap src={"plots/map_white.html"} />
+                    }
                 </Grid>
 
                 <TextWithAudio text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ornare eros id odio auctor, sed consectetur nunc bibendum. Sed tristique ex et tempus ultrices. In feugiat at lorem eget malesuada. Sed nec arcu a lectus malesuada eleifend non quis purus. Phasellus in eros a ex finibus luctus. Maecenas ac odio dictum, ultrices nunc vel, faucibus felis. Ut et urna nisl. Cras non molestie massa, sit amet aliquam nibh. Donec vel vestibulum orci, in placerat ipsum."}/>
